96f07358bf9b0b5793c099511940bbd770f0bb1f
rambius <rambiusparkisanius@gmail.com>
2017-03-06 08:50:05 +0200

    added mock test foor read_file

--- sharkan_test.py
+++ sharkan_test.py
@@ -4,17 +4,23 @@ import os.path
 import sharkan
 import tempfile
 import unittest
+import unittest.mock
 
 class SharkanTest(unittest.TestCase):
 
   def setUp(self):
     self.file = tempfile.mkstemp()[1]
+    self.pyed = sharkan.PyEd()
 
-  def test_write_read(self):
-    pyed = sharkan.PyEd()
-    pyed.write_to_file(self.file, "test text")
-    txt = pyed.read_file(self.file)
+  def test_write_read_read(self):
+    self.pyed.write_to_file(self.file, "test text")
+    txt = self.pyed.read_file(self.file)
     self.assertEqual(txt, "test text")
+
+  @unittest.mock.patch('builtins.open', new_callable=unittest.mock.mock_open, read_data='test')
+  def test_read_file_mock(self, m):
+    txt = self.pyed.read_file("mock")
+    self.assertEqual(txt, 'test')
 
   def tearDown(self):
     if os.path.exists(self.file):
